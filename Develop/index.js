2a9295c88f0c152ddc129669b0e788d8f9accece
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-04-03 10:34:31 -0700

    wrote function to save svg image to a logo.svg file that also includes error func and  console log success message

--- Develop/index.js
+++ Develop/index.js
@@ -46,4 +46,17 @@ inquirer
         charPreset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
         ignoreChars: '0o1i',
       }).data;
+
+    // Save the SVG image to a file (logo.svg)
+        //How: write the generated SVG image to a file named logo.svg using the fs.writeFile() method, and log a message to the console indicating whether the file was written successfully or not.
+        // fs.writeFile() method used to write data to a file
+        // logo.svg = arg 1 = filename of the file to be written
+        // svg = arg 2 = data to be written to the file (svg variable that holds the generated SVG image)
+        // (err) =>  = callback function that is called when file is written or an error occurs
+        // If there is an error, the throw keyword will immediately stop the execution of the code and throw an error.
+    fs.writeFile('logo.svg', svg, (err) => {
+    if (err) throw err;
+    // If there is no error a message will be printed
+    console.log('Logo saved to logo.svg');
+    });
   });
