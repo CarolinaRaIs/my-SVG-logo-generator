dc5f05d71f1e2a41992aa0e691550f14b861a649
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-06-26 16:06:45 -0700

    Fixed repeated txt bug in createLogo.js and updated circle.js for clarity

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-SVG-logo-generator
 A Node.js command-line application that takes in user input to generate a logo and save it as an SVG file.
+
+The SVG Logo Generator is a command-line application that allows users to easily create unique logos for their projects or businesses. With this app, users can generate SVG-based logos with customizable shapes, colors, and text.
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 // Import necessary packages and files
+//const createLogo = require('./lib/createLogo');
+//const writeToFile = require('./lib/fileWriter');
+
+//async function generateAndSaveLogo() {
+  //try {
+    //const svgWithText = await createLogo();
+    //writeToFile('logo.svg', svgWithText);
+    //console.log('Logo saved to logo.svg');
+  //} catch (error) {
+    //console.error(error);
+  //}
+//}
+
+//generateAndSaveLogo();
+
+
+
+
+// Import necessary packages and files
 const createLogo = require('./lib/createLogo');
 const writeToFile = require('./lib/fileWriter');
 
 async function generateAndSaveLogo() {
   try {
-    const svgWithText = await createLogo();
-    writeToFile('logo.svg', svgWithText);
+    const svg = await createLogo(); // Generate the logo SVG without text
+    const text = 'ABC'; // Replace 'ABC' with your desired text
+    const textColor = 'red'; // Replace 'red' with your desired text color
+
+    const svgWithText = createLogo.addText(svg, text, textColor); // Add the text to the SVG
+
+    writeToFile('logo.svg', svgWithText); // Save the modified SVG to a file
     console.log('Logo saved to logo.svg');
   } catch (error) {
     console.error(error);
@@ -13,3 +37,4 @@ async function generateAndSaveLogo() {
 }
 
 //generateAndSaveLogo();
+
